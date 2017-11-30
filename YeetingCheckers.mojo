1ddbf46e5e0a0435e44f0fb769a58a434b90b262
tianlerk <tian_lerkhoong@mymail.sutd.edu.sg>
2017-11-30 21:09:12 +0800

    Added and tested button_sensing

--- YeetingCheckers.mojo
+++ YeetingCheckers.mojo
@@ -2,9 +2,14 @@
 <project name="YeetingCheckers" board="Mojo V3" language="Lucid">
   <files>
     <src top="true">mojo_top.luc</src>
+    <src>button_sensing.luc</src>
     <src>led_multiplexer.luc</src>
+    <ucf>buttons.ucf</ucf>
     <ucf>led.ucf</ucf>
     <ucf lib="true">mojo.ucf</ucf>
+    <component>pipeline.luc</component>
+    <component>button_conditioner.luc</component>
     <component>reset_conditioner.luc</component>
+    <component>edge_detector.luc</component>
   </files>
 </project>
--- work/verilog/mojo_top_0.v
+++ work/verilog/mojo_top_0.v
@@ -18,7 +18,9 @@ module mojo_top_0 (
     output reg avr_rx,
     input avr_rx_busy,
     output reg [5:0] row,
-    output reg [5:0] col
+    output reg [5:0] col,
+    input [5:0] buttoncol,
+    input [5:0] buttonrow
   );
   
   
@@ -46,6 +48,25 @@ module mojo_top_0 (
     .timerout(M_led_multiplexer_timerout)
   );
   
+  wire [3-1:0] M_button_sensing_a_row;
+  wire [3-1:0] M_button_sensing_a_col;
+  wire [3-1:0] M_button_sensing_b_row;
+  wire [3-1:0] M_button_sensing_b_col;
+  wire [1-1:0] M_button_sensing_read;
+  reg [6-1:0] M_button_sensing_buttonrow;
+  reg [6-1:0] M_button_sensing_buttoncol;
+  button_sensing_3 button_sensing (
+    .clk(clk),
+    .rst(rst),
+    .buttonrow(M_button_sensing_buttonrow),
+    .buttoncol(M_button_sensing_buttoncol),
+    .a_row(M_button_sensing_a_row),
+    .a_col(M_button_sensing_a_col),
+    .b_row(M_button_sensing_b_row),
+    .b_col(M_button_sensing_b_col),
+    .read(M_button_sensing_read)
+  );
+  
   always @* begin
     M_reset_cond_in = ~rst_n;
     rst = M_reset_cond_out;
@@ -53,12 +74,14 @@ module mojo_top_0 (
     spi_miso = 1'bz;
     spi_channel = 4'bzzzz;
     avr_rx = 1'bz;
-    M_led_multiplexer_inp_int[0+5-:6] = 6'h0c;
-    M_led_multiplexer_inp_int[6+5-:6] = 6'h0c;
-    M_led_multiplexer_inp_int[12+5-:6] = 6'h0c;
-    M_led_multiplexer_inp_int[18+5-:6] = 6'h0c;
-    M_led_multiplexer_inp_int[24+5-:6] = 6'h3f;
-    M_led_multiplexer_inp_int[30+5-:6] = 6'h3f;
+    M_button_sensing_buttoncol = buttoncol;
+    M_button_sensing_buttonrow = buttonrow;
+    M_led_multiplexer_inp_int[0+5-:6] = M_button_sensing_a_col;
+    M_led_multiplexer_inp_int[6+5-:6] = M_button_sensing_a_row;
+    M_led_multiplexer_inp_int[12+5-:6] = buttoncol;
+    M_led_multiplexer_inp_int[18+5-:6] = 6'h3f;
+    M_led_multiplexer_inp_int[24+5-:6] = M_button_sensing_b_col;
+    M_led_multiplexer_inp_int[30+5-:6] = M_button_sensing_b_row;
     row = M_led_multiplexer_row;
     col = M_led_multiplexer_column;
     led[0+0-:1] = M_led_multiplexer_timerout;
